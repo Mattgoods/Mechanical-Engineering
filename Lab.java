97d59af88ce72f8e731476abb0e3bdc3f7c4c6db
Matt G <Mgoodman624@gmail.com>
2022-03-14 21:21:57 -0400

    Now able to perform subtraction.

--- Lab.java
+++ Lab.java
@@ -11,9 +11,17 @@ public class Lab {
         System.out.print("Enter second operand: ");
         double secondOperand = scnr.nextDouble();
         System.out.println("");
+        System.out.print("Enter 1 for Addition or 2 for Subtraction: ");
+        int choice = scnr.nextInt();
+        System.out.println("");
 
-        double result = firstOperand + secondOperand;
-        System.out.print("Result of Addition: " + result);
-
+        if (choice == 1) {
+            double result = firstOperand + secondOperand;
+            System.out.print("Result of Addition: " + result);
+        }
+        if (choice == 2) {
+            double result = firstOperand - secondOperand;
+            System.out.print("Result of Subtraction: " + result);
+        }
     }
 }
